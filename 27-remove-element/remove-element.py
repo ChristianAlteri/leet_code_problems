6a7c7afd69e4e72b677cb51bdf9c73b836199dfa
Christian Alteri <120008055+ChristianAlteri@users.noreply.github.com>
2024-01-16 14:35:38 +0000

    Time: 15 ms (76.49%) | Memory: 13.2 MB (62.07%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums.sort()
+        while val in nums:
+            nums.remove(val)
+
+        # print(nums)
+        return len(nums)
